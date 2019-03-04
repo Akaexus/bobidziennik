32d8843266a6338f413bfb3d421c50a58541e764
kak <kak500@wp.pl>
2019-03-04 21:14:23 +0100

    Finished studentStats

--- controllers/studentStats.php
+++ controllers/studentStats.php
@@ -5,17 +5,34 @@ class StudentStats extends Controller
 {
 	public function marksOverview()
 	{
-		$student = Student::load($this->studentId);
-		$studentClass = $student->getClass();
-		// print_r($studentClass);
-		$studentLeader = Teacher::load($studentClass->wychowawca);
-		// print_r($studentLeader);
+		$student = Student::load($this->studentId);					// pobranie danych ucznia
+		$studentClass = $student->getClass();						// pobranie informacji o klasie
+		$studentLeader = Teacher::load($studentClass->wychowawca);	// pobranie informacji o wychowawcy
+
+		$subjects = array_map(function($student) {					// pobranie informacji o przedmiotach
+			return new Subject($student);
+		}, DB::i()->select([
+			'select'=> '*',
+			'from'=> Subject::$databaseTable
+		]));
+
+
+		$marks = array_map(function($student) {						// pobranie informacji o ocenach
+			return new Mark($student);
+		}, DB::i()->select([
+			'select'=> '*',
+			'from'=> Mark::$databaseTable,
+			'where'=> "id_ucznia=".$this->studentId
+		]));
+
 
 		$template = Output::i()->getTemplate('StudentStats', 'stats');
 		Output::i()->add($template->render([
 			'student'=> $student,
 			'studentClass'=> $studentClass,
-			'studentLeader'=> $studentLeader
+			'studentLeader'=> $studentLeader,
+			'subjects'=> $subjects,
+			'marks'=> $marks
 		]));
 	}
 
@@ -31,7 +48,6 @@ class StudentStats extends Controller
 	}
 
 }
-
 
 
 ?>
