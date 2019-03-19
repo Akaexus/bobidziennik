d807cb80ff2f88eb1ff41c553c85519c4583c55b
kak500 <kak500@wp.pl>
2019-03-19 20:11:13 +0100

    footer modified

--- src/index.js
+++ src/index.js
@@ -1,2 +1,12 @@
 import './styles/main.scss';
-//
+let footer = document.querySelector("footer");
+let header = document.querySelector("header");
+let body = document.getElementsByTagName('body');
+
+let footerHeight = footer.clientHeight;
+let headerHeight = header.clientHeight;
+console.log(footerHeight);
+console.log(headerHeight);
+// console.log(header);
+
+body.style.height=100%-(footerHeight+headerHeight);
